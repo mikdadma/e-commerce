59ccb5eb0527fd5a15917005e6441f69c76253f7
mikdad ma <mikdadma12@gmail.com>
2026-09-15 09:19:57 +0530

    feat: implement logout functionality using Redux for state management

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,10 +1,13 @@
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logout } from "../redux/slices/authSlice";
 
 function Logout() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleLogout = () => {
-    localStorage.removeItem("user");
+    dispatch(logout());
     navigate("/login");
   };
 
